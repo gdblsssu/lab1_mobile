1726a96c1592c4fdaa2cbfe79a00e005520b7148
gdblsssu <112731291+gdblsssu@users.noreply.github.com>
2023-12-15 18:04:02 +0300

    Update Common.kt

--- app/src/main/java/com/example/lab0/api/Common.kt
+++ app/src/main/java/com/example/lab0/api/Common.kt
@@ -3,5 +3,5 @@ package com.example.lab0.api
 object Common {
 
     val retrofitService: ApiService
-        get() = RetrofitHelper.getClient("http://192.168.0.199:8080/").create(ApiService::class.java)
+        get() = RetrofitHelper.getClient("").create(ApiService::class.java)
 }
